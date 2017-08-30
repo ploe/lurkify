06aa33803287480ab0d3e83d7c67c2881ffeb1d6
Myke Atkinson <ploe@hotmail.co.uk>
2017-08-30 19:45:24 +0000

    Connnects to mongo

--- main.rb
+++ main.rb
@@ -1,8 +1,19 @@
+require 'mongo'
 require 'sinatra'
 
 set :bind, '0.0.0.0'
 set :server, :puma
 
+mongodb = nil
+before do
+	mongodb = Mongo::Client.new(['127.0.0.1:27017'], :database => 'lurkify')
+end
+
+
 get '/' do
-      'hello, world'
+	out = ''
+	mongodb[:parties].find.each do |party|
+		out += party.to_s + "\n"
+	end
+	out
 end
